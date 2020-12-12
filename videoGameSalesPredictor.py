df38561694d38305b8a85001ff6c23f2203e8dbe
Olcay <34381831+olcayarpac@users.noreply.github.com>
2020-12-12 13:59:39 +0300

    Update videoGameSalesPredictor.py

--- videoGameSalesPredictor.py
+++ videoGameSalesPredictor.py
@@ -16,6 +16,7 @@ def isNumericalString(s):
 data = pd.read_csv('video-game-sales-data.csv')
 
 # visualize critic score point and global sales relationship
+fig = plt.figure(dpi=1200)
 plt.scatter(data['Critic_Score'], data['Global_Sales'], s=10)
 plt.ylabel('Global_Sales', fontsize=13)
 plt.xlabel('Critic_Score', fontsize=13)
@@ -26,7 +27,8 @@ plt.show()
 data = data.drop(data[(data['Critic_Score']>60) & (data['Global_Sales']>60)].index)
 
 # by plotting again, we can see that we got rid of from an outlier
-
+'''
+fig = plt.figure(dpi=1200)
 plt.scatter(data['Critic_Score'], data['Global_Sales'], s=10)
 plt.ylabel('Global_Sales', fontsize=13)
 plt.xlabel('Critic_Score', fontsize=13)
@@ -41,6 +43,7 @@ plt.scatter(data['Year_of_Release'], data['Global_Sales'], s=10)
 plt.ylabel('Global_Sales', fontsize=13)
 plt.xlabel('Year_of_Release', fontsize=13)
 plt.show()
+'''
 
 # most of the data in 'User_Score' column is not numerical, so convert them
 
@@ -61,7 +64,7 @@ plt.xlabel('User_Score', fontsize=13)
 plt.show()
 
 
-
+'''
 for index, row in data.iterrows():
     if row['Rating'] == 'E10+':
         data.at[index, 'Rating'] = 'E10'
@@ -70,7 +73,7 @@ groupedByRating = data.groupby('Rating')
 meanRating = groupedByRating.mean()
 meanRating = meanRating.reset_index()
 
-fig = plt.figure()
+fig = plt.figure(dpi=1200)
 plt.bar(meanRating['Rating'], meanRating['User_Score'])
 plt.ylim(ymin=40)
 plt.xlabel('Content Rating') 
@@ -78,13 +81,14 @@ plt.ylabel('Average User Score')
 plt.title('Average User Scores of Content Rating Categories') 
 plt.show() 
 
-fig = plt.figure()
+fig = plt.figure(dpi=1200)
 plt.bar(meanRating['Rating'], meanRating['Global_Sales'])
 plt.xlabel('Content Rating') 
 plt.ylabel('Average Global Sale (Millions)') 
 plt.title('Average Global Sale Amount of Content Rating Categories') 
 plt.show() 
-    
+'''
+# mark platform as other
 
 data.loc[data['Platform'].isin((data['Platform'].value_counts()
                                 [data['Platform'].value_counts() < 50]).index), 'Platform'] = 'Other'
@@ -92,30 +96,89 @@ data.loc[data['Platform'].isin((data['Platform'].value_counts()
 groupedByPlatform = data.groupby('Platform')
 sumPlatform = groupedByPlatform.sum().reset_index()
 meanPlatform = groupedByPlatform.mean().reset_index()
-
-fig = plt.figure()
+'''
+fig = plt.figure(dpi=1200)
 plt.bar(sumPlatform['Platform'], sumPlatform['Global_Sales'])
 plt.xlabel('Platform') 
 plt.xticks(rotation='vertical')
-plt.ylabel('Total Global Sales(Millions)') 
+plt.ylabel('Total Global Sales (Millions)') 
 plt.title('Total Global Sale Amount of Platforms') 
 plt.show() 
 
-fig = plt.figure()
+fig = plt.figure(dpi=1200)
 plt.bar(meanPlatform['Platform'], meanPlatform['Global_Sales'])
 plt.xlabel('Platform') 
 plt.xticks(rotation='vertical')
-plt.ylabel('Average Global Sales(Millions)') 
+plt.ylabel('Average Global Sales (Millions)') 
 plt.title('Average Global Sale Amount of Platforms') 
 plt.show() 
 
+fig = plt.figure(dpi=1200, figsize=(16,8))
+ax1 = plt.subplot(121, aspect='equal')
+sumPlatform.plot(kind='pie', y = 'Global_Sales', ax=ax1, autopct='%1.1f%%', 
+        startangle=90, shadow=False, labels=sumPlatform['Platform'], legend = False, 
+        title='Global Sale Distribution and Platform', fontsize=10)
+plt.show()
+'''
+
+# genre visualization
+
+groupedByGenre = data.groupby('Genre')
+sumGenre = groupedByGenre.sum().reset_index()
+meanGenre = groupedByGenre.mean().reset_index()
+
+fig = plt.figure(dpi=1200)
+plt.bar(sumGenre['Genre'], sumGenre['Global_Sales'])
+plt.xlabel('Genre') 
+plt.xticks(rotation='vertical')
+plt.ylabel('Total Global Sales (Millions)') 
+plt.title('Global Sale Amount and Genre ') 
+plt.show() 
+
+
+fig = plt.figure(dpi=1200)
+plt.bar(sumGenre['Genre'], meanGenre['User_Score'])
+plt.xlabel('Genre') 
+plt.xticks(rotation='vertical')
+plt.ylabel('Average User Score') 
+plt.ylim(ymin=60)
+plt.title('Average User Score and Genre ') 
+plt.show() 
+
+fig = plt.figure(dpi=1200, figsize=(16,8))
+ax1 = plt.subplot(121, aspect='equal')
+sumGenre.plot(kind='pie', y = 'Global_Sales', ax=ax1, autopct='%1.1f%%', 
+        startangle=90, shadow=False, labels=sumGenre['Genre'], legend = False, 
+        title='Global Sales Distribution and Genre', fontsize=10)
+plt.show()
+
+# TOP 100 Selled Games
+
+top100 = data.sort_values('Global_Sales',ascending = False).head(100)
+top100GroupedGenre = top100.groupby('Genre')
+top100GroupedPublisher = top100.groupby('Publisher')
+top100SumGenre = top100GroupedGenre.sum().reset_index()
+top100SumPublisher = top100GroupedPublisher.sum().reset_index()
+
+fig = plt.figure(dpi=1200, figsize=(16,8))
+ax1 = plt.subplot(121, aspect='equal')
+top100SumGenre.plot(kind='pie', y = 'Global_Sales', ax=ax1, autopct='%1.1f%%', 
+        startangle=90, shadow=False, labels=top100SumGenre['Genre'], legend = False, 
+        title='TOP 100 Selled Game, Sales Distribution and Genre', fontsize=10)
+plt.show()
+
+fig = plt.figure(dpi=1200, figsize=(16,8))
+ax1 = plt.subplot(121, aspect='equal')
+top100SumPublisher.plot(kind='pie', y = 'Global_Sales', ax=ax1, autopct='%1.1f%%', 
+        startangle=90, shadow=False, labels=top100SumPublisher['Publisher'], legend = False, 
+        title='TOP 100 Selled Game, Sale Distributions and Publisher Company', fontsize=10)
+plt.show()
 
 
 
-data2 = pd.read_csv('video-game-sales-data.csv')
 
 
-
+#data.to_csv('video-game-sales-data.csv', index=False)
 
 
 
